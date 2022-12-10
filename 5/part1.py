21453fa047647abc980597bd4d793c8f80a69b8a
Gary Reynolds <gary@touch.asn.au>
2022-12-10 23:50:54 +1100

    add day 5 part 1

---
+++ 5/part1.py
@@ -0,0 +1,22 @@
+import sys
+import re
+
+stacks = {}
+
+for line in sys.stdin:
+    if not line[:-1]:
+        break
+    crates = line[1::4]
+    for stack, crate in enumerate(crates, 1):
+        if re.search(r"[ \d]", crate):
+            continue
+        stacks.setdefault(stack, []).insert(0, crate)
+
+
+for line in sys.stdin:
+    n, source, dest = [int(d) for d in re.findall(r"\d+", line)]
+    for i in range(n):
+        stacks[dest].append(stacks[source].pop())
+
+
+print("".join([stacks[i][-1] for i in sorted(stacks)]))
